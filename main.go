40ed9a1d49a322d57e0daa3b5eac0bc434db00a8
AmirBagjani <amirbagjani@gmail.com>
2024-05-15 17:41:18 +0330

    main go ignored

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-
+import (
+    "bufio"
+    "os"
+)
 
 func main() {
+    f := os.Stdout
 
+    w := bufio.NewWriterSize(f, 3)
+
+    // Will print abc. d is added the buffer and will not be printed without an explicit .Flush()
+    w.WriteString("abc")
+    w.WriteString("b")
+    // w.WriteString("c")
+    // -----------------
+    // w.WriteString("d")
+	w.Flush()
 }
