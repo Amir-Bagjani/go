d57a7ff69fcceed9bae0f5030386473a0d0395ea
AmirBagjani <amirbagjani@gmail.com>
2024-05-15 17:40:54 +0330

    main go

--- main.go
+++ main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-    "bufio"
-    "os"
-)
+
 
 func main() {
-    f := os.Stdout
 
-    w := bufio.NewWriterSize(f, 3)
-
-    // Will print abc. d is added the buffer and will not be printed without an explicit .Flush()
-    w.WriteString("abc")
-    w.WriteString("b")
-    // w.WriteString("c")
-    // -----------------
-    // w.WriteString("d")
-	w.Flush()
 }
