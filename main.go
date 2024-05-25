011e0ebc65576bcac432d13ad1a13061e07b7f17
AmirBagjani <amirbagjani@gmail.com>
2024-05-25 16:26:36 +0330

    add simple test

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+func IntMin(n1, n2 int) int {
+	if n1 < n2 {
+		return n1
+	} else {
+		return n2
+	}
+}
 
 func main() {
-	dir, err := os.Getwd()
-	fmt.Println(dir,err)
+	// fmt.Println(IntMin(10,20))
 }
