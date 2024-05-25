81d805ebbdcaa1ef4a1954e13a9ac76be5cb9e7e
AmirBagjani <amirbagjani@gmail.com>
2024-05-25 17:24:36 +0330

    add filepath

--- main.go
+++ main.go
@@ -1,13 +1,42 @@
 package main
 
-func IntMin(n1, n2 int) int {
-	if n1 < n2 {
-		return n1
-	} else {
-		return n2
-	}
-}
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
 
 func main() {
-	// fmt.Println(IntMin(10,20))
+	p := filepath.Join("dir1", "dir2", "filename")
+	fmt.Println(p)
+
+	fmt.Println(filepath.Join("dir1//", "filename"))
+	fmt.Println(filepath.Join("dir1/../dir1", "filename"))
+
+	fmt.Println(filepath.Base(p))
+	fmt.Println(filepath.Dir(p))
+
+	d, f := filepath.Split(p)
+	fmt.Println("directory =>", d, "base is =>", f)
+
+	fmt.Println(filepath.IsAbs("/dir"))
+	fmt.Println(filepath.IsAbs("dir"))
+
+	filename := "config.json"
+	exc := filepath.Ext(filename)
+	fmt.Println(exc)
+	fmt.Println(strings.TrimSuffix(filename, exc))
+
+	rel, err := filepath.Rel("a/b", "a/b/t/file")
+	if err != nil {
+        panic(err)
+    }
+    fmt.Println(rel)
+
+	rel, err = filepath.Rel("a/b", "a/c/t/file")
+	if err != nil {
+        panic(err)
+    }
+    fmt.Println(rel)
+
 }
