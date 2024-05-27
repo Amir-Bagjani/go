e7ab7ca4e66119f83e39b54a46eb166e74b12ed8
AmirBagjani <amirbagjani@gmail.com>
2024-05-27 13:20:53 +0330

    add slice section

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"strings"
-)
-
-const str = "Reading is my...\r\n favourite"
-
 func main() {
-	fmt.Println("length of reader is", len(str))
-
-	stringReader := strings.NewReader(str)
-
-	bs := bufio.NewScanner(stringReader)
-
-	for bs.Scan() {
-		fmt.Printf("The output is %q \n", bs.Text())
-	}
+	
 }
